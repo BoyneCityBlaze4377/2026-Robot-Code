6d9846ac727b2e65d9b35fa7d504f939e7aaea62
BoyneCityBlaze4377 <158527092+BoyneCityBlaze4377@users.noreply.github.com>
2026-03-28 08:34:26 -0400

    stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -152,6 +152,7 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     // return new PathPlannerAuto("HopClimb");
+    // return new PathplannerAuto("");
     return null;
   }
 }
